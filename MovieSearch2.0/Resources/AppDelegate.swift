da2415bc712bd0c6bde309c09fda610a9adccec8
AdamAM <adamam@gmail.com>
2018-05-26 20:21:57 -0600

    Testing git

--- MovieSearch2.0/Resources/AppDelegate.swift
+++ MovieSearch2.0/Resources/AppDelegate.swift
@@ -17,8 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
 //        MovieController.fetchMovies(searchTerm: "fish") { (Movie) in
-//            
+//
 //        }
+        
+        // Testing github
         
         return true
     }
